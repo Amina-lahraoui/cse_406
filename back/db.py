6a46e4ace636ccd490e9ee8657ea29d5913314c8
belarbi-leo <leomehdibelarbi@gmail.com>
2025-11-21 14:48:44 +0900

    docs: docs: add pdf, change port front for deploy http part2..

--- back/db.py
+++ back/db.py
@@ -1,10 +1,23 @@
-from sqlalchemy import create_engine
+import os
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-# docker exec -it cse_406-db-1 psql -U user -d mydb
 DATABASE_URL = os.getenv("DATABASE_URL")
+DB_NAME = "rds-prod-cse-406"
+DATABASE_URL_DEFAULT = DATABASE_URL.rsplit("/", 1)[0] + "/postgres"
+
+engine_default = create_engine(DATABASE_URL_DEFAULT, isolation_level="AUTOCOMMIT")
+
+with engine_default.connect() as conn:
+    result = conn.execute(text(f"SELECT 1 FROM pg_database WHERE datname='{DB_NAME}'"))
+    exists = result.scalar()
+    
+    if not exists:
+        conn.execute(text(f'CREATE DATABASE "{DB_NAME}"'))
+        print(f"Database '{DB_NAME}' created successfully!")
+    else:
+        print(f"Database '{DB_NAME}' already exists.")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
