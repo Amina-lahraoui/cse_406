4dddefa10d6b954faf25d99ee42f28c2f4c24633
belarbi-leo <leomehdibelarbi@gmail.com>
2025-11-21 13:51:18 +0900

    docs: add pdf, change port front for deploy http

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
-import App from '@/app'
+import App from '@/App'
 import '@/index.css'
 import "@/i18n";
 
